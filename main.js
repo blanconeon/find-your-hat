55384d744505e173273baae654f536b23f3a16ed
Emmanuel B <blancocemmanuel@gmail.com>
2026-08-24 06:01:16 +1000

    comit

--- main.js
+++ main.js
@@ -92,6 +92,18 @@ class Field {
      console.log(this.stringField()); // always prints the CURRENT, up-to-date grid
   
   }
+  
+  static generateField(height, width, percentage) {
+  let generatedField = [];
+  for (let i = 0; i < height; i++) {
+   generatedField.push([]);
+  for (let j=0; j < width; j ++) {
+    
+  } 
+}   
+
+}
+
 }
 
 // function to create a loop using a condition// 
